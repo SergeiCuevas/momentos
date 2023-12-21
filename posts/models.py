565cad79591bc0765d3f9ce90da2a89b5fad4438
SergioAndroide <sergeicuevas1@gmail.com>
2023-12-20 21:02:23 -0600

    maquetado y estilos

--- posts/models.py
+++ posts/models.py
@@ -9,7 +9,12 @@ class Post(models.Model):
     created_at = models.DateField(auto_now_add=True)
     quote = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.quote
+
 class Comment(models.Model):
-    models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments', null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments', null=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments", null=True )
     text = models.TextField()
+
+    
